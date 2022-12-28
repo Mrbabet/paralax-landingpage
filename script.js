df2a0141b61af0ba5d1b2ad541b5f17658702e93
Mrbabet <wieliczkokamil@gmail.com>
2022-12-28 11:14:46 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let text = document.getElementById("text");
+let bird1 = document.getElementById("bird1");
+let bird2 = document.getElementById("bird2");
+let rocks = document.getElementById("rocks");
+let water = document.getElementById("water");
