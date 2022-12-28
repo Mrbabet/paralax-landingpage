c3d5089169e38c535df58e5bd664a474f49d4ce6
Mrbabet <wieliczkokamil@gmail.com>
2022-12-28 13:06:04 +0100

    Need to work on fonts and breakpoints. Paralax is working

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ let bird1 = document.getElementById("bird1");
 let bird2 = document.getElementById("bird2");
 let rocks = document.getElementById("rocks");
 let water = document.getElementById("water");
+let forest = document.getElementById("forest");
+let header = document.getElementById("header");
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+
+  text.style.top = 50 + value * -0.3 + "%";
+  bird1.style.top = value * -1.5 + "px";
+  bird1.style.left = value * 2 + "px";
+  bird2.style.top = value * -1.5 + "px";
+  bird2.style.left = value * -5 + "px";
+  rocks.style.top = value * -0.12 + "px";
+  forest.style.top = value * 0.25 + "px";
+  header.style.top = value * 0.7 + "px";
+});
